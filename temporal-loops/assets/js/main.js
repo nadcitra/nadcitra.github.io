f26ac41a7d4a974dbb26d0c300b7b2e8b79b6479
nadcitra <nadcitra@users.noreply.github.com>
2020-06-19 08:35:57 +1000

    0619

--- temporal-loops/assets/js/main.js
+++ temporal-loops/assets/js/main.js
@@ -5,4 +5,18 @@ $(document).ready(function(){
 		$('.toggle').slideToggle("fast");
 	});
  
+$(document).ready(function () {
+  $(".menu").click(function () {
+    $(".essay").addClass("slide");
+    $(".close").addClass("show");
+  });
+
+  $(".close").click(function () {
+    $(".essay").removeClass("slide");
+    $(".close").removeClass("show");
+  });
+
+
+
+
 });
