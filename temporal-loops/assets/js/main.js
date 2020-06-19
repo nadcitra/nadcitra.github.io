3d1a746ec991f02c4de92bff1e4c810c48873406
nadcitra <nadcitra@users.noreply.github.com>
2020-06-19 10:29:28 +1000

    0619

--- temporal-loops/assets/js/main.js
+++ temporal-loops/assets/js/main.js
@@ -1,21 +1,37 @@
 $(document).ready(function(){
 	console.log("I'm Ready")
-
-	$('.header').click(function(){
-		$('.toggle').slideToggle("fast");
-	});
  
-$(document).ready(function () {
-  $(".menu").click(function () {
-    $(".essay").addClass("slide");
-    $(".close").addClass("show");
+  $(".s-1").click(function () {
+    $(".p-1").addClass("show");
   });
 
-  $(".close").click(function () {
-    $(".essay").removeClass("slide");
-    $(".close").removeClass("show");
+  $(".p-1-close").click(function () {
+    $(".p-1").removeClass("show");
   });
 
+  $(".s-2").click(function () {
+    $(".p-2").addClass("show");
+  });
+
+  $(".p-2-close").click(function () {
+    $(".p-2").removeClass("show");
+  });
+
+  $(".s-3").click(function () {
+    $(".p-3").addClass("show");
+  });
+
+  $(".p-3-close").click(function () {
+    $(".p-3").removeClass("show");
+  });
+
+  $(".s-4").click(function () {
+    $(".p-4").addClass("show");
+  });
+
+  $(".p-4-close").click(function () {
+    $(".p-4").removeClass("show");
+  });
 
 
 
